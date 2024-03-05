1f9ef48bbdf91f00dc03e0784edfe3f47839d8b9
ANKIT KUMAR <codingank@gmail.com>
2024-03-05 20:46:37 +0530

    project

--- src/Components/ProjectDetails.js
+++ src/Components/ProjectDetails.js
@@ -7,9 +7,9 @@ const ProjectDetails=()=>{
 return(
 <>
 <div className="w-screen aspect-video   absolute text-white  bg-gradient-to-r from-black top-0 ">
-    <div className="bg-gradient-to-l from-black">
+    <div className="bg-gradient-to-l from-black p-2">
   <Link to={"/"}> <div className="text-white text-center font-base sm:font-bold text-xs  sm:text-sm bg-red-600 rounded-lg p-1 sm:p-3 py-2 inline-block fixed">Home</div></Link> 
-  <p className="text-indigo-500 ml-12 sm:ml-24 bg-black font-bold p-2 rounded-lg text-[.3em] sm:text-sm fixed">Api frequently changes so might be live-website  not work properly so please visit <span className="text-white"> linkedIn-Thought</span> </p>
+  <p className="text-indigo-500 ml-12 sm:ml-24 bg-black font-bold p-2 rounded-lg text-xs sm:text-sm fixed">Api frequently changes so might be live-website  not work properly so please visit <span className="text-white"> linkedIn-Thought</span> </p>
 {
     myProject.map(item=><ProjectInfo data={item}/>)
 }
