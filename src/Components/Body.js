af9614f347e99dfbe05e47bb8124107838779203
ANKIT KUMAR <codingank@gmail.com>
2024-03-05 20:30:57 +0530

    changed ui

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -45,27 +45,27 @@ const Body=()=>{
         <>
      <div className='text-lime-600  text-sm lg:text-base absolute top-6 right-[40%] p-10 '>{msg}</div>
         <div className=" text-lg text-white " >
-            <h1 className="p-2 px-6 font-bold">ChatGpt <span className="text">3.5</span></h1>
+            <h1 className="p-2 px-6 font-bold">ChatGpt <span className="text">3.5  </span></h1>
         </div>
 
          <div className="flex justify-center flex-col ">
-            <div className="pt-52 lg:pt-36 flex flex-col justify-center">
+            <div className="pt-72 lg:pt-36 flex flex-col justify-center">
 
                <div className=""> <img className="w-9  bg-white rounded-md  m-auto" src="gpt.png" alt='img-of-gpt'></img> </div>
                <div> <h2 className="text-white font-bold text-xl pt-4 text-center">How can I help you today ?</h2></div>
 
             </div>
              
-        <div className="text-white flex flex-col justify-center  m-auto md:m-20 mb-6  md:mb-0 pt-24 lg:pt-0">
+        <div className="text-white flex flex-col justify-center   md:m-20 mb-6  md:mb-0 pt-24 lg:pt-0">
 
-          <div className="flex flex-col md:flex-row justify-center pt-8 md:pt-0">
-        <Link to={"/project"}> <div className=" w-72 mb-2 md:mb-0  p-1 px-4 pr-32 border  rounded-xl bordergpt  text-xs sm:text-sm gpthead hover:bg-[#424242] hover:cursor-pointer ">check my project <h5 className="text-xs gpt">just click one time</h5>  </div> </Link> 
-       <Link to={"contact"}>   <div className="ml-0 md:ml-2 w-72 p-1 px-4 pr-32 border  rounded-xl bordergpt  text-xs sm:text-sm gpthead hover:bg-[#424242] hover:cursor-pointer  ">contact me  <h5 className="text-xs gpt">to know about me</h5>    </div></Link>
+          <div className="w-full flex flex-col md:flex-row justify-center pt-8 md:pt-0">
+        <Link to={"/project"}> <div className="w-full md:w-72 mb-2 md:mb-0  p-1 px-4 pr-32 border  rounded-xl bordergpt  text-base sm:text-sm gpthead hover:bg-[#424242] hover:cursor-pointer ">check my project <h5 className="text-xs gpt">just click one time</h5>  </div> </Link> 
+       <Link to={"contact"}>   <div className="ml-0 md:ml-2 w-full md:w-72 p-1 px-4 pr-32 border  rounded-xl bordergpt  text-base sm:text-sm gpthead hover:bg-[#424242] hover:cursor-pointer  ">contact me  <h5 className="text-xs gpt">to know about me</h5>    </div></Link>
           </div>
 
-          <div className="flex flex-col md:flex-row justify-center  pt-2">
-         <Link to={"/skill"}><div className="w-72 mb-2 md:mb-0 p-1 px-4 pr-32 border  rounded-xl bordergpt  text-xs sm:text-sm gpthead hover:bg-[#424242] hover:cursor-pointer ">check my skill <h5 className="text-xs gpt">top-notch skill</h5></div></Link> 
-          <div className="hidden sm:block ml-0 md:ml-2  w-72 p-1 px-4 pr-32 border  rounded-xl bordergpt  text-xs sm:text-sm gpthead hover:bg-[#424242] hover:cursor-not-allowed ">upcoming Event <h5 className="text-xs gpt">planning for upcoming  </h5></div>
+          <div className="w-full flex flex-col md:flex-row justify-center  pt-2">
+         <Link to={"/skill"}><div className="w-full md:w-72 mb-2 md:mb-0 p-1 px-4 pr-32 border  rounded-xl bordergpt  text-base sm:text-sm gpthead hover:bg-[#424242] hover:cursor-pointer ">check my skill <h5 className="text-xs gpt">top-notch skill</h5></div></Link> 
+          <div className="hidden sm:block ml-0 md:ml-2 w-full md:w-72 p-1 px-4 pr-32 border  rounded-xl bordergpt  text-base sm:text-sm gpthead hover:bg-[#424242] hover:cursor-not-allowed ">upcoming Event <h5 className="text-xs gpt">planning for upcoming  </h5></div>
           </div>
 
         </div>
