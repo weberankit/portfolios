f75fab48978223352d5c433db81b91cb62ee530c
ANKIT KUMAR <codingank@gmail.com>
2024-03-06 10:02:04 +0530

    bg-color

--- src/Components/ProjectDetails.js
+++ src/Components/ProjectDetails.js
@@ -9,15 +9,15 @@ const ProjectDetails=()=>{
       {
       main:"Api frequently changes so might be live-website  not work properly so please visit",
       sub:"linkedIn-Thought",
-      mainColor:"indigo-500",
-      subColor:"white"
+      mainColor:"text-indigo-500",
+      subColor:"text-white"
     }
     ,
     {
       main:"To view project video on large screen and for more controll on video click on ",
       sub:"Video",
-      mainColor:"white",
-      subColor:"indigo-500"
+      mainColor:"text-white",
+      subColor:"text-indigo-500"
 
     }
   ]
@@ -27,6 +27,7 @@ const ProjectDetails=()=>{
 function callRandom(){
   const randomNo=Math.floor(Math.random()*impMsg.length)
   setInd(()=>impMsg[randomNo])
+ // console.log("hi")
 }
 useEffect(()=>{
 const timer=setInterval(()=>{
@@ -40,7 +41,7 @@ return(
 <div className="w-screen aspect-video   absolute text-white  bg-gradient-to-r from-black top-0 ">
     <div className="bg-gradient-to-l from-black p-2">
   <Link to={"/"}> <div className="text-white text-center font-base sm:font-bold text-xs  sm:text-sm bg-red-600 rounded-lg p-1 sm:p-3 py-2 inline-block fixed">Home</div></Link> 
-  <p className={` text-${ind.mainColor} ml-12 sm:ml-24 bg-black font-bold p-2 rounded-lg text-xs sm:text-sm fixed`}>{ind.main} <span className={`text-${ind.subColor}`}> {ind.sub}</span> </p>
+  <p className={` ${ind.mainColor} ml-12 sm:ml-24 bg-black font-bold p-2 rounded-lg text-xs sm:text-sm fixed`}>{ind.main} <span className={`${ind.subColor}`}> {ind.sub}</span> </p>
 {
     myProject.map((item,index)=><ProjectInfo key={index} data={item}/>)
 }
