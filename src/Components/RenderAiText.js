264c83c9a3e003041708a73fe5379cc4d9e694a6
ANKIT KUMAR <codingank@gmail.com>
2024-08-01 18:11:49 +0530

    whole app ai functionalities changed and used redux

---
+++ src/Components/RenderAiText.js
@@ -0,0 +1,74 @@
+import { useEffect, useRef, useState } from "react"
+import { category } from "../constantCategory"
+
+import { memo } from "react"
+const RenderAiText=memo( ({selectData , scrollTo})=>{
+ 
+   const [getData,setgetData] =useState(null)
+   const [imgPreview, setPreview] = useState(false)
+
+
+   
+
+
+useEffect(()=>{
+    if(selectData){
+        const result = category.filter((item)=>{
+       
+         
+            
+      if(selectData.toLowerCase().trim().includes(item.categ.toLowerCase().trim())){
+          // console.log(item.categ)
+              return item
+            }
+    })
+
+    setgetData(()=>result)
+
+    
+
+   
+}
+
+//console.log("hi",selectData)
+
+},[selectData])
+
+ 
+
+
+if(!selectData){
+    return null
+}
+
+function handleopenLink(link){
+    window.open(link,"_blank")
+}
+
+    return(
+        <>
+        {
+getData && <div>
+    <div className="  ">
+        
+         <div>
+           <div onClick={()=>setPreview(!imgPreview)} className={` hover:cursor-pointer ${imgPreview?"w-full":"w-44"} rounded-lg`}><img className="rounded-2xl" src={getData[0]?.img1} alt={"image"}></img></div>
+            <div className="bg-black px-2 py-1 rounded-lg text-white font-serif w-20 mt-1 hover:bg-red-600 "><button onClick={()=>handleopenLink(getData[0]?.link)}>open</button></div>
+             </div>
+<div >{getData[0]?.description}</div>
+
+
+    </div>
+
+          </div>
+
+
+}
+
+<div id={scrollTo}></div>
+
+
+        </>
+    )
+})
+export default RenderAiText
--- src/customHooks/GenAi.js
+++ src/customHooks/GenAi.js
@@ -1,29 +1,54 @@
 
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-export const GenAi=(question,data,setData,setIndi)=>{
+
+export const GenAi=(question,userInput,setIndi,setError,dispatch,addData)=>{
     const apiKey=process.env.REACT_APP_API_KEY;
-    const genAI = new GoogleGenerativeAI(apiKey);
+   const genAI = new GoogleGenerativeAI(apiKey);
+  
   async  function apiCall(){
         try{
             // For text-only input, use the gemini-flash-model
+    
             const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
           
             const prompt = `
-          ${question} ${data}
+          ${question} ${userInput?.current?.value }
               `
           //console.log(prompt ,"prompt")
             const result = await model.generateContent(prompt);
             const response = await result.response;
             const text = response.text();
-           setData((prev)=>[...prev,{ans:text}])
+            console.log(text,response)
+           if(text){
+            console.log("hi i am rounning")
+            dispatch(addData(text))
+           // setData((prev)=>[...prev,{ans:text}])
+          setIndi(false)
+          setError(null)
+         userInput.current.value="" 
+      
+          
+           } 
+          
+     }catch(error){
            setIndi(false)
-        //   console.log(data,"ff")
-          }catch(error){
-           setData((prev)=>[...prev,{ans:"something went wrong please refresh page and if error continue then please inform me"}])
+           setError(`something went wrong(use different prompt ) please ask again and if error continue then please inform me`)
+           console.log(error)
+           dispatch(addData("error"))
+           userInput.current.value="" 
           }
+
+
+
+
+
+
+
+
     }
 
 apiCall()
 
 }
+
